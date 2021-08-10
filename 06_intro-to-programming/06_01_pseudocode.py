7ece278d55ff768ce917e3c85b9420841f7cdced
Leena <engleena@yahoo.com>
2021-08-10 21:39:03 +0300

    psudeo code cute cats

--- 06_intro-to-programming/06_01_pseudocode.py
+++ 06_intro-to-programming/06_01_pseudocode.py
@@ -6,3 +6,11 @@
 # Break this task up into a couple of steps of pseudocode
 # and write the pseudocode below in code comments.
 # You don't need to write any functional code, just map out the steps.
+##### my code starts down ################
+# ----------------------------------------#
+# collect the names of the cats and save them
+#  check if each cat is cute
+#  show a message that each cute cat is ready for adoption
+#
+#
+#
