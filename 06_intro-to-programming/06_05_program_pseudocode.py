308a6015159098c276cf8447feac7fb84bfda504
Leena <engleena@yahoo.com>
2021-08-10 21:45:42 +0300

    6 is completed

--- 06_intro-to-programming/06_05_program_pseudocode.py
+++ 06_intro-to-programming/06_05_program_pseudocode.py
@@ -6,3 +6,11 @@
 # You can also add real Python code after learning the necessary concepts.
 # For now, just practice breaking larger tasks into smaller steps
 # and writing out your thoughts in pseudocode.
+##### my code starts below #########
+# the task is to update calendar with all upcoming meeting invites from yahoo ###
+# ------------------------------------------------------------------------- ##
+# login to yahoo mail with the appropriate credentials
+# check inbox if there are any upcoming meeting invites
+# update calender with the details of each calender invite
+# notify user with SMS that calender has been recently updated
+#
