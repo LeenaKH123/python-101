eccd9d9b12af4560815405f87cd11d68ba99a7cd
Leena <engleena@yahoo.com>
2021-08-18 15:53:28 +0300

    strings in progress

--- 09_strings-and-text/09_08_edibles.py
+++ 09_strings-and-text/09_08_edibles.py
@@ -1,4 +1,4 @@
-# Extract four words of edible food items from the sentence below.
+# Extract four ss of edible food items from the sentence below.
 # Use only string slicing to pick them out!
 # Feel free to use pen and paper to number the indices
 # and find the locations quicker.
@@ -6,3 +6,21 @@
 # What dish can you make from these ingredients? :)
 
 s = "They grappled with their leggins before going to see the buttercups flourish."
+print(
+    s[26]
+    + s[27]
+    + s[28]
+    + " "
+    + s[57]
+    + s[58]
+    + s[59]
+    + s[60]
+    + s[61]
+    + s[62]
+    + " "
+    + s[68]
+    + s[69]
+    + s[70]
+    + s[71]
+    + s[72]
+)
