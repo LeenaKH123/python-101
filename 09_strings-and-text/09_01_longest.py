3ffe876365421deda0cc70b179a00210fe729ed8
Leena <engleena@yahoo.com>
2021-08-18 18:07:14 +0300

    string updated

--- 09_strings-and-text/09_01_longest.py
+++ 09_strings-and-text/09_01_longest.py
@@ -9,14 +9,19 @@ print("german word length", len(longest_german_word))
 print("hungarian word length", len(longest_hungarian_word))
 print("finish word length", len(longest_finnish_word))
 print("strong password length", len(strong_password))
+test_list = [
+    longest_german_word,
+    longest_hungarian_word,
+    longest_finnish_word,
+    strong_password,
+]
+max_length = -1
+for char in test_list:
+    if len(char) > max_length:
+        res = char
+print("longest string is : " + res)
 print(
-    "the longest string is",
-    max(
-        longest_german_word,
-        longest_hungarian_word,
-        longest_finnish_word,
-        strong_password,
-    ),
+    "the length of the longest string is",
     max(
         len(longest_german_word),
         len(longest_hungarian_word),
