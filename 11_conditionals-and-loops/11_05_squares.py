92071a741668d4951e1a5da46fa709c8f363f483
Leena <engleena@yahoo.com>
2021-08-19 18:43:39 +0300

    section 11 in progress

--- 11_conditionals-and-loops/11_05_squares.py
+++ 11_conditionals-and-loops/11_05_squares.py
@@ -1,2 +1,5 @@
 # Write a script that prints out all the squares of numbers from 1 to 50.
 # Use a `for` loop that demonstrates the use of the `range()` function.
+x = range(1, 50)
+for n in x:
+    print(n ** 2)
