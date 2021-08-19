8046fe6c9ec9798ba6ee55af44d7392287302cc6
Leena <engleena@yahoo.com>
2021-08-19 10:53:35 +0300

    cmpleted 3 excercises in section 10

--- 10_operators-and-booleans/10_02_hello_five_times.py
+++ 10_operators-and-booleans/10_02_hello_five_times.py
@@ -1,2 +1,4 @@
 # Write the necessary code to display "Hello World!" five times.
 # TIP: Use a mathematical operator on the string!
+math_statement = "Hello World!" * 5
+print(math_statement)
--- 10_operators-and-booleans/10_03_temp_converter.py
+++ 10_operators-and-booleans/10_03_temp_converter.py
@@ -2,3 +2,6 @@
 # ----------------------
 # Write the necessary code to convert a degree in Fahrenheit
 # to Celsius and print it to the console. Use variable names!
+fahrenhite = 112
+celsius = (fahrenhite - 32) * (5 / 9)
+print("tempreture in celsius is", celsius)
