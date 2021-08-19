b7ee363ec688aca3dec6072613a7e4483dad0169
Leena <engleena@yahoo.com>
2021-08-19 11:40:09 +0300

    section 11 in progress

--- 11_conditionals-and-loops/11_02_vowel_ipsum.py
+++ 11_conditionals-and-loops/11_02_vowel_ipsum.py
@@ -1,5 +1,8 @@
 # Print the total number of vowels that are used in the lorem ipsum text.
 
+from typing import Counter
+
+
 lorem_ipsum = """Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
 sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
 Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut 
@@ -7,3 +10,10 @@ aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
 voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
 Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt 
 mollit anim id est laborum."""
+
+vowels = "aeoiu"
+counter = 0
+for char in lorem_ipsum:
+    if char in vowels:
+        counter += 1
+print("the number of vowels is", counter)
