4b1acbea05de33c9f865e9d69a4986323155a078
Leena <engleena@yahoo.com>
2021-08-19 11:24:28 +0300

    section 10 is almost finished

--- 10_operators-and-booleans/10_04_modulo_magic.py
+++ 10_operators-and-booleans/10_04_modulo_magic.py
@@ -5,3 +5,17 @@ num_1 = 42
 num_2 = 137
 num_3 = 455
 num_4 = 1997
+
+res1 = num_1 % 7
+res2 = num_2 % 7
+res3 = num_3 % 7
+res4 = num_4 % 7
+
+if res1 == 0:
+    print("This number is divisible by 7:", num_1)
+if res2 == 0:
+    print("This number is divisible by 7:", num_2)
+if res3 == 0:
+    print("This number is divisible by 7:", num_3)
+if res4 == 0:
+    print("This number is divisible by 7:", num_4)
--- 10_operators-and-booleans/10_06_wool_and_bools.py
+++ 10_operators-and-booleans/10_06_wool_and_bools.py
@@ -1,2 +1,10 @@
 # Write a code snippet that gives a name to a `sheep`
 # and uses a boolean value to define whether it has `wool` or not.
+sheep = "shaun with wool"
+bool1 = None
+if "wool" in sheep:
+    bool1 = True
+else:
+    bool1 = False
+if bool1:
+    print("the sheep is with wool")
