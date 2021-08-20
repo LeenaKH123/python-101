72362e8e3dc428a0a9465ab6b8c523a99a2739e3
Leena <engleena@yahoo.com>
2021-08-20 10:05:19 +0300

    section 11 prime numbers in progress

--- 11_conditionals-and-loops/11_09_primes.py
+++ 11_conditionals-and-loops/11_09_primes.py
@@ -1,4 +1,6 @@
 # Print out every prime number between 1 and 1000.
-x =  range (1, 10)
-for n in range:
-    if 
+for y in range(1, 10):
+    if y % 2 == 0:
+        print(y, "is not prime")
+    else:
+        print(y, "is prime")
