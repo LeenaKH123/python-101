38a69e7611f8468a86f95357524f780e2b343164
Leena <engleena@yahoo.com>
2021-08-20 12:01:58 +0300

    fizz buzz completed

--- 11_conditionals-and-loops/11_11_fizzbuzz.py
+++ 11_conditionals-and-loops/11_11_fizzbuzz.py
@@ -4,3 +4,14 @@
 # - If the number is divisible by 3, write `Fizz` instead of the number
 # - If the number is divisible by 5, write `Buzz` instead of the number
 # - If the number is divisible by both 3 and 5, write `FizzBuzz` instead of the number
+n = 16
+number = range(1, n)
+for x in number:
+    if x % 5 == 0 and x % 3 == 0:
+        print("FizzBuzz")
+    elif x % 5 == 0:
+        print("Buzz")
+    elif x % 3 == 0:
+        print("Fizz")
+    else:
+        print(str(x))
