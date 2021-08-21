1cae89f686d16f0caa21579ca12bf4f3323bda0f
Leena <engleena@yahoo.com>
2021-08-21 15:14:09 +0300

    section 13 in progress

--- 13_modules-and-automation/13_02_the_time_please.py
+++ 13_modules-and-automation/13_02_the_time_please.py
@@ -1,2 +1,6 @@
 # Use a built-in Python module to tell you the current date and time.
 # Research online, so you can print it in a readable manner.
+import datetime, time
+
+time1 = time.strftime("%H:%M:%S:%MS", time.localtime())
+print(time1)
